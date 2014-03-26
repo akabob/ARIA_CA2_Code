6df2dab6a037502fc7b50605fc73cdc429059efe
akabob <mcswebtech2014@gmail.com>
2014-03-26 20:57:09 +0000

    ARIA Project Seventh Commit

---
+++ js/Calculator.js
@@ -0,0 +1,63 @@
+﻿var Calc = (function () {
+
+    return {
+        AddNo: function (opOne, opTwo) {
+            var oOne = parseInt(opOne);
+            var oTwo = parseInt(opTwo);
+            if (isNaN(oOne) || isNaN(oTwo)) {
+                throw new Error("Not a Number");
+            }
+            var answer = oOne + oTwo
+
+            return answer;
+
+        },
+
+        SubNo: function (opOne, opTwo) {
+            var oOne = parseInt(opOne);
+            var oTwo = parseInt(opTwo);
+            if (isNaN(oOne) || isNaN(oTwo)) {
+                throw new Error("Not a Number");
+            }
+            var answer = oOne - oTwo
+
+            return answer;
+
+        },
+        MultplyNo: function (opOne, opTwo) {
+
+
+            if (isNaN(opOne) || isNaN(opTwo)) {
+                throw new Error("Not a Number");
+            }
+
+            var answer = opOne * opTwo
+
+            return answer;
+
+        },
+
+
+        DivideNo: function (opOne, opTwo) {
+
+            if (isNaN(opOne) || isNaN(opTwo)) {
+                throw new Error("Not a Number");
+            }
+
+            if (opOne == 0 || opTwo == 0) {
+
+                throw new Error("Divide by Zero");
+            }
+
+
+            var answer = opOne / opTwo
+
+            return answer;
+
+        },
+
+        // formats to two decimal places    
+        formatTwo: function (n) { n = parseFloat(n); return isNaN(n) ? false : n.toFixed(2); }
+
+    }
+})();// end of 
--- js/sk1.js
+++ js/sk1.js
@@ -13,7 +13,101 @@
 
 };
 
-    
+$(document).ready(function () {
+    $("#convertbutton").click(function () {
+        var metricValue = $("#inputID").val();
+        // var metricValue = $("#metricinput").val();
+        var imperialValue = $("#imperialinput").val();
+        var ValueToConvert = $("#inputID").val();
+        var selected = $('#conversion :selected').val();
+        $("#ansID").val(calculate(selected, ValueToConvert));       // puts the convertion into the correct dom element on the page.
+
+        //  $("#ansID").val(Converter.convertFromMetricToImperial(metricValue));
+
+        if (((metricValue.length != 0) && (imperialValue.length != 0))
+            || ((metricValue.length == 0) && imperialValue.length == 0)) {
+            alert("Please set one field to convert")
+        }
+        if (metricValue.length != 0) {
+            $("#imperialinput").val(Converter.convertFromPoundsToKIlo(metricValue));
+        }
+        else if (imperialValue.length != 0) {
+            $("#metricinput").val(Converter.convertFromMetricToImperial(imperialValue));
+        }
+
+
+
+    });
+    $("#clearbutton").click(function () {
+        $("#inputIDt").val('');
+        $("#ansID").val('');
+    });
+
+    $("#pnlRemove").click(function () {
+        /* this function remove the fron panelof the cube so you can see inside and changes the bottom image which is the manchester United chrest with the image that ]
+        was the front panel image]  this only shows the panels being changed by using code*/
+
+        var imageName = 'img/v8liv.PNG';
+
+        var changepnl = $('#btmpnl');
+        var pnlID = $('#v8front');
+
+
+
+        $(pnlID).hide();                       // hide the front panel
+        $('#btmpnl').attr('src', imageName);        // change the bottom image to v8rear.PNG
+
+
+
+    });
+
+
+
+
+    $('#mathFunction :selected').val();
+    $('#mathFunction').change(function () {
+
+        /* this fucntion calls the calucate funtion with the number to be converted  with the conversion type which comes from the select tag, eg pk is pounds to kilo's
+        this function fires when the select dropdown box changes */
+        var opOne = $("#opOne").val();
+        var opTwo = $("#opTwo").val();
+        var selected = $('#mathFunction :selected').val();
+
+
+        //  console.log($('#conversion :selected').val());              //write it out to the console to verify what was 
+
+        $("#resultID").val(process(selected, opOne,opTwo));       // puts the convertion into the correct dom element on the page.
+    }).change();
+
+});
+
+function process(selected, opOne,opTwo) {
+
+
+
+    switch (selected) {
+        case "+":
+            return Calc.AddNo(opOne,opTwo);
+            break;
+        case "-":
+            return Calc.SubNo(opOne, opTwo);
+            break;
+        case "/":
+            return Calc.DivideNo(opOne, opTwo);
+            break;
+        case "*":
+            return Calc.MultplyNo(opOne, opTwo);
+            break;
+       
+
+        default:
+
+            return "Error ! ";
+
+            //  code to be executed if n is different from case 1 and 2
+    }
+
+
 
 
 }
