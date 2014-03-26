0dad566890eb5acb943a6930c9d9d37968b34900
akabob <mcswebtech2014@gmail.com>
2014-03-26 19:45:16 +0000

    ARIA Project Forth Commit

--- test/js/Calculator.js
+++ test/js/Calculator.js
@@ -1,1 +1,58 @@
-﻿
+﻿var Calc = (function () {
+
+    return {
+        AddNo: function (opOne, opTwo) {
+            var oOne = parseInt(opOne);
+            var oTwo = parseInt(opTwo);
+            if (isNaN(oOne) || isNaN(oTwo)) {
+                throw new Error("Not a Number !");
+            }
+            var answer = oOne + oTwo
+
+            return answer;
+
+        },
+
+        SubNo: function (opOne, opTwo) {
+            var oOne = parseInt(opOne);
+            var oTwo = parseInt(opTwo);
+            if (isNaN(oOne) || isNaN(oTwo)) {
+                throw new Error("Not a Number !");
+            }
+            var answer = oOne - oTwo
+
+            return answer;
+
+        },
+        MultplyNo: function (opOne, opTwo) {
+
+
+            if (isNaN(opOne) || isNaN(opTwo)) {
+                throw new Error("Not a Number !");
+            }
+
+            var answer = opOne * opTwo
+
+            return answer;
+
+        },
+
+
+        DivideNo: function (opOne, opTwo) {
+
+            if (isNaN(opOne) || isNaN(opTwo)) {
+                throw new Error("Not a Number");
+            }
+
+
+            var answer = opOne / opTwo
+
+            return answer;
+
+        },
+
+        // formats to two decimal places    
+        formatTwo: function (n) { n = parseFloat(n); return isNaN(n) ? false : n.toFixed(2); }
+
+    }
+})();// end of 
